9f2bfc0bb72636fac63e5fa9d0819cc5f0a300b5
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2024-03-22 11:16:18 +0100

    Update Navbar.tsx

--- Task Manager/src/Components/Navbar.tsx
+++ Task Manager/src/Components/Navbar.tsx
@@ -1,36 +1,34 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  let navlinks = [
+    { text: "Home" },
+    { text: "About" },
+    { text: "Contact" },
+    { text: "Login" },
+  ];
+
   return (
     <>
-      <nav className="bg-gray-800 py-4">
-        <div className="container mx-auto flex justify-between items-center">
+      <nav className="bg-gray-800 py-4 drop-shadow-[0_5px_5px_rgba(31,41,55,1)] rounded-b-xl">
+        <div className="mx-auto flex justify-between items-center px-10">
           {/* Project Name */}
-          <Link to="/" className="text-white hover:text-gray-300">
+          <Link to="/" className="text-white hover:text-gray-300 text-4xl">
             Task Manager
           </Link>
 
           {/* Navigation Links */}
           <ul className="flex space-x-4">
             <li>
-              <Link to="/" className="text-white hover:text-gray-300">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className="text-white hover:text-gray-300">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="/contact" className="text-white hover:text-gray-300">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link to="/login" className="text-white hover:text-gray-300">
-                Login
-              </Link>
+              {navlinks.map((nav) => (
+                <Link
+                  className="text-white hover:bg-white/25 duration-300 p-5 rounded-xl"
+                  to={nav.text === "Home" ? "/" : `/${nav.text.toLowerCase()}`}
+                  key={nav.text}
+                >
+                  {nav.text}
+                </Link>
+              ))}
             </li>
           </ul>
         </div>
